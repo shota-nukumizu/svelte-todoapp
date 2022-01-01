9fabd61f2415e8bf51c9fe6908f7e238f16e13bc
shota-nukumizu <author2000.1225@gmail.com>
2022-01-02 07:41:28 +0900

    create todo app

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,61 @@
 <script lang="ts">
-	let title = ''
+  let title = '';
+  let todoList: string[] = [];
 
-	// 作成ボタンを押したときの処理
-	const handleClickCreateButton = () => {
-		alert(title)
-		title = ''
-	}
+  $: disabledCreateButton = title === '';
+
+  // 作成ボタンを押したときの処理
+  const handleClickCreateButton = () => {
+    todoList = [...todoList, title];
+    title = '';
+  };
+
+  // 完了処理
+  const completeItem = (index) => {
+    todoList = todoList.filter((_, i) => i !== index);
+  };
 </script>
 
 <main>
-	<label for="">
-		タイトル
-		<input bind:value={title} />
-	</label>
-	<button on:click={handleClickCreateButton}>作成</button>
+	<div class="container mt-3">
+		<div class="row justify-content-center">
+			<div class="col-lg-6 col-md-8">
+			<h2>TODO アプリ</h2>
+			<form>
+				<div class="form-group">
+				<label for="title">タイトル</label>
+				<input id="title" class="form-control" bind:value={title} />
+				</div>
+				<div class="form-group text-center">
+				<button
+					class="btn btn-primary px-5"
+					disabled={disabledCreateButton}
+					on:click={handleClickCreateButton}>
+					作成
+				</button>
+				</div>
+			</form>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-lg-6 col-md-8">
+			{#if todoList.length === 0}
+				<div>アイテムを作成してください</div>
+			{:else}
+				<ul class="list-group">
+				{#each todoList as todoItem, index}
+					<li class="list-group-item align-middle">
+					{todoItem}
+					<button
+						class="btn btn-sm btn-success float-right"
+						on:click={() => completeItem(index)}>
+						×
+					</button>
+					</li>
+				{/each}
+				</ul>
+			{/if}
+			</div>
+		</div>
+	</div>
 </main>
-
-<style>
-</style>
